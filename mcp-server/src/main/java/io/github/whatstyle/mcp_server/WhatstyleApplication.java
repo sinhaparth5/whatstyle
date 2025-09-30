ad0be6c6e3ce7ff27124beb46b85e863916356d2
Parth Sinha <parth.sinha@webcurl.co.uk>
2025-09-30 15:06:08 +0100

    added mcp server

---
+++ mcp-server/src/main/java/io/github/whatstyle/mcp_server/WhatstyleApplication.java
@@ -0,0 +1,13 @@
+package io.github.whatstyle.mcp_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhatstyleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhatstyleApplication.class, args);
+	}
+
+}
